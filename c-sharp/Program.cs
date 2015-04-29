b183ade3b88c6e7608af326b1aebe6125ecc473c
briankoksal <brian.koksal@gmail.com>
2015-04-28 20:35:20 -0400

    mychanges

---
+++ c-sharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //implement an algorithm to determine if string has all unique characters
+
+            Console.WriteLine(AllUniqueCharacters("hello"));
+            Console.ReadLine();
+        }
+
+        private static bool AllUniqueCharacters(string value)
+        {
+            List<Char> characters = new List<char>();
+
+            for (int x = 0; x < value.Count(); x++)
+            {
+                char cur = value.ElementAt(x);
+                if (characters.Contains(cur))
+                {
+                    return false;
+                }
+                else {
+                    characters.Add(cur);
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
